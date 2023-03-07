97b395159e647e5bc338cd5ee727dfffe64fc1a7
HLHHS11 <chiryucity@gmail.com>
2023-03-07 21:37:30 +0900

    230307 githubを利用する方針に変えたので一旦コミット

--- src/controller.js
+++ src/controller.js
@@ -1,2 +1,17 @@
+import {Info} from "./typedef.js"
 // リクエストからルーティングされることになっている。
 // ルーティングによって呼ばれると、Modelからデータを取得したり更新したりして、viewを返す
+
+export class Controller {
+    /**
+     * データを作成するようModelに要請し、得られた結果をviewに渡す
+     * @param {Info} info 
+     */
+    static create (info) {
+        // スプシとの通信をModelに実装。Model
+        // イメージ：Model.create(hoge);
+
+        // viewに情報を渡す
+
+    }
+}
